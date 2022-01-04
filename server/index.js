5bafe091ebde659054d1f50df636aaf52bbbe61d
killerfrost19 <95533178+killerfrost19@users.noreply.github.com>
2022-01-04 18:11:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,9 +17,10 @@ app.use(bodyParser.urlencoded({limit:"50mb",extended:true}));
 
 app.use("/posts",postRoutes)
 
-const CONNECTION_URL="mongodb+srv://varsha:memories1912@mempro.jf0ao.mongodb.net/MEMORIES?retryWrites=true&w=majority"
+const CONNECTION_URL="MONGO_DB URL"
 const PORT=process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
     .then(()=>app.listen(PORT,()=> console.log("server listening on port "+PORT)))
     .catch((error)=> console.log(error))
+
